275cebb04a5f27780b876430f482574d6ffcac1b
IlanP <ilan.petiot@icloud.com>
2022-09-27 10:14:27 +0200

    🎓 - Python Types - Intro: Simple types
    And add comments

--- main.py
+++ main.py
@@ -13,15 +13,27 @@ async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
 
+# Python Types - Intro #
+
+# Motivation #
 def get_full_name(first_name: str, last_name: str):
     full_name = first_name.title() + " " + last_name.title()
     return full_name
 
 
+# More Motivation #
 def get_name_with_age(name: str, age: int):
     name_with_age = name + " is this old: " + str(age)
     return name_with_age
 
 
+# Declaring types #
+
+# Simple types #
+def get_items(item_a: str, item_b: int, item_c: float, item_d: bool, item_e: bytes):
+    return item_a, item_b, item_c, item_d, item_d, item_e
+
+
 print(get_full_name("john", "doe"))
 print(get_name_with_age("john", 20))
+print(get_items("Hello", 1, 2.0, True, b"World"))

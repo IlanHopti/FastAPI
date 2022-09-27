cc336db5e3b1a6dbd41997ca3b280efaaaeebfc0
IlanP <ilan.petiot@icloud.com>
2022-09-27 10:01:39 +0200

    🎓 - Python Types - Intro : More Motivation
    Add second exercice

--- main.py
+++ main.py
@@ -18,4 +18,10 @@ def get_full_name(first_name: str, last_name: str):
     return full_name
 
 
+def get_name_with_age(name: str, age: int):
+    name_with_age = name + " is this old: " + str(age)
+    return name_with_age
+
+
 print(get_full_name("john", "doe"))
+print(get_name_with_age("john", 20))

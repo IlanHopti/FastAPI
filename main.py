092a129fb029691d1ab3b1775e3bf1585266ed4d
IlanP <ilan.petiot@icloud.com>
2022-09-27 09:59:05 +0200

    🎓 - Python Types - Intro : Motivation
    adding first exemple in Intro

--- main.py
+++ main.py
@@ -11,3 +11,11 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+def get_full_name(first_name: str, last_name: str):
+    full_name = first_name.title() + " " + last_name.title()
+    return full_name
+
+
+print(get_full_name("john", "doe"))

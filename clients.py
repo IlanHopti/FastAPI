fd465790f11a24e6d1a89b6aee1f8e220ca3eca4
Infernaton <72390824+Infernaton@users.noreply.github.com>
2022-09-28 21:23:41 +0200

    create the delete commands and the delete clients

--- clients.py
+++ clients.py
@@ -11,6 +11,11 @@ app = FastAPI()
 data = json.load(open("antique_dealer.json"))
 
 
+def write_db(data2):
+    with open("antique_dealer.json", "w", encoding='utf-8') as f:
+        json.dump(data2, f, ensure_ascii=False, indent=4)
+
+
 class Client(BaseModel):
     client_id: UUID
     client_first_name: str
@@ -21,7 +26,22 @@ class Client(BaseModel):
 @router.post("/add")
 async def create_client(client: Client):
     data['clients'].append(client.dict())
-    return {"client" : client }
+    return {"client": client}
+
+
+@router.delete("/{client_id}")
+async def delete_client_by_id(client_id: int):
+    return {"client": [f"Suppression du client n°{client_id}"]}
+
+
+@router.delete("/{client_id}")
+async def delete_client_by_id(client_id: str):
+    data.json.load(open("antique_dealer.json"))
+    for command in data['clients']:
+        if command['client_id'] == client_id:
+            data['clients'].remove(command)
+            write_db(data)
+            return {"client": [f"Client n°{client_id} deleted"]}
 
 
 @router.get("/")
--- commands.py
+++ commands.py
@@ -10,13 +10,28 @@ app = FastAPI()
 data = json.load(open("antique_dealer.json"))
 
 
+def write_db(data2):
+    with open("antique_dealer.json", "w", encoding='utf-8') as f:
+        json.dump(data2, f, ensure_ascii=False, indent=4)
+
+
 # Commands #
 
 @router.get("/{command_id}")
-async def get_antique_dealer_by_id(command_id: int):
+async def get_command_by_id(command_id: int):
     for command in data['commands']:
         if command['command_id'] == command_id:
             return command
+
+
+@router.delete("/{command_id}")
+async def delete_command_by_id(command_id: str):
+    data.json.load(open("antique_dealer.json"))
+    for command in data['commands']:
+        if command['command_id'] == command_id:
+            data['commands'].remove(command)
+            write_db(data)
+            return {"command": [f"Command n°{command_id} deleted"]}
 
 
 # Create a new command #

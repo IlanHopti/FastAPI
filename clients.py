555dfac7e5763fa79fb668cf439566864ad36a92
SELMImohamed <91205436+SELMImohamed@users.noreply.github.com>
2022-09-29 18:59:44 +0200

    add delete client 🚶‍♂️
    delete a client from the json

--- clients.py
+++ clients.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,APIRouter
+from fastapi import FastAPI, APIRouter,HTTPException
 from pydantic import BaseModel
 from uuid import UUID
 import json
@@ -11,6 +11,11 @@ app = FastAPI()
 data = json.load(open("antique_dealer.json"))
 
 
+def write_db(data2):
+    with open("antique_dealer.json", "w", encoding='utf-8') as f:
+        json.dump(data2, f, ensure_ascii=False, indent=4)
+
+
 class Client(BaseModel):
     client_id: UUID
     client_first_name: str
@@ -21,9 +26,22 @@ class Client(BaseModel):
 @router.post("/add")
 async def create_client(client: Client):
     data['clients'].append(client.dict())
-    return {"client" : client }
+    return {"client": client}
+
+
+@router.delete("/{client_id}")
+async def delete_client(client_id: int):
+    data = json.load(open("antique_dealer.json"))
+    for client in data['clients']:
+        if client["client_id"] == client_id:
+            data['clients'].remove(client)
+            write_db(data)
+        return {f"the {client_id} as been deleted"}
+    raise HTTPException(status_code=404, detail="user not found")
+
 
 
 @router.get("/")
 async def get_client():
+    data = json.load(open("antique_dealer.json"))
     return data['clients']

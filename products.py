f1bdb2b3a18cdb4cd10cf888d212cedca8c203b1
IlanP <ilan.petiot@icloud.com>
2022-09-30 11:12:26 +0200

    📝 - Change post commands
    - Add def to get last command_id
    -Add condition if command_id in delete and patch

--- products.py
+++ products.py
@@ -8,10 +8,8 @@ router = APIRouter(
 )
 app = FastAPI()
 
-data = json.load(open("antique_dealer.json"))
 
 # function for writing in json file
-
 
 def write_db(data2):
     with open("antique_dealer.json", "w", encoding='utf-8') as f:
@@ -21,7 +19,7 @@ def write_db(data2):
 # create new product
 
 class Product(BaseModel):
-    product_id: int
+    product_id: int | None = None
     product_name: str
     product_description: str
     product_year: int
@@ -29,26 +27,46 @@ class Product(BaseModel):
     product_price: float
 
 
+def get_last_product_id():
+    data = json.load(open("antique_dealer.json"))
+    return data["products"][-1]["product_id"]
+
+
 # function for get product
 
 @router.get("/{product_id}")
 async def read_products(product_id: int):
+    data = json.load(open("antique_dealer.json"))
     for product in data["products"]:
         if product["product_id"] == product_id:
             return product
+    if product_id not in data["products"]:
+        return {"message": f"Product_id : {product_id} not found"}
 
 
 # function for add new product
-@router.post("/")
-async def create_product(product: Product):
+@router.post("/{product_id}")
+async def create_product(product_id: int, product: Product):
+    id_taken = False
     data = json.load(open("antique_dealer.json"))
-    data['products'].append(product.dict())
-    write_db(data)
-    return data['products']
+    product.product_id = product_id
+    for products in data["products"]:
+        if product.product_id == products["product_id"]:
+            product.product_id = get_last_product_id() + 1
+            id_taken = True
+    if id_taken:
+        data["products"].append(product.dict())
+        write_db(data)
+        return {"created product": f"The id {product.product_id} was already taken,"
+                                   f" your product id has been changed to {product.product_id}",
+                "new product": product}
+    else:
+        data["products"].append(product.dict())
+        write_db(data)
+        return {"created product": product}
 
 
 # function for delete product
-
 
 @router.delete("/{product_id}")
 async def delete_product(product_id: int):
@@ -58,5 +76,5 @@ async def delete_product(product_id: int):
             data['products'].remove(product)
             write_db(data)
             return {"message": f"Product {product_id} deleted"}
-
-
+    if product_id not in data['products']:
+        return {"message": f"Product {product_id} not found"}
